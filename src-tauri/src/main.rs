2f6bfc2a388137c5ab62e2a031549217d90ecfa9
hklee <leehunkyu00@gmail.com>
2022-08-01 04:52:43 +0900

    add debugtool code in rust

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,19 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::Manager;
+
 fn main() {
   tauri::Builder::default()
+    // .setup(|app| {
+    //   #[cfg(debug_assertions)]
+    //   {
+    //     let window = app.get_window("main").unwrap();
+    //     window.open_devtools();
+    //     window.close_devtools();
+    //   }
+    //   Ok(())
+    // })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
